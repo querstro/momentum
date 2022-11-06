666a3cb40ea65304fde07eb0a64121fd22064844
querstro <85439942+querstro@users.noreply.github.com>
2022-11-06 20:58:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    renderCurrentTime()
-    renderQuote()
+    renderCurrentTime();
+    renderQuote();
+    renderRandomImage();
 });
 //현재 시간
 function renderCurrentTime() {
@@ -22,3 +23,15 @@ function renderQuote() {
             $('#author').text(author)
         })
 }
+// 랜덤 사진 꾸미기
+function renderRandomImage() {
+    let imageList = [];
+    // 이미지 개수를 변경하려면 i=5의 값을 이미지 개수만큼 바꿔주세요!
+    for (i = 0; i < 7; i++) {
+      imageList.push(i);
+    }
+    let imageListLength = imageList.length;
+    let randomImage = Math.floor(Math.random() * (imageListLength))+1
+    randomImage = `images/${randomImage}.jpg`
+    $(document.body).css("background-image", `url(${randomImage})`);
+  }
